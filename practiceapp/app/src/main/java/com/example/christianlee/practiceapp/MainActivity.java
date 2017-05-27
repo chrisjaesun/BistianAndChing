8bbaed6b70469c909a3ac5ffdf7dc6a6c0eb218f
Christian Lee <christianlee@Christians-iMac.local>
2017-05-26 20:29:00 -0700

    bug fix where bing sucks button goes off screen

--- practiceapp/app/src/main/java/com/example/christianlee/practiceapp/MainActivity.java
+++ practiceapp/app/src/main/java/com/example/christianlee/practiceapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.christianlee.practiceapp;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Display;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void BingReallySucks(View view) {
-        view.setY(view.getY() +  50);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int screen_w = dm.widthPixels;
+        int screen_h = dm.heightPixels;
+
+        int resId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resId > 0) {
+            screen_h -= getResources().getDimensionPixelSize(resId);
+        }
+
+        TypedValue typedValue = new TypedValue();
+        if(getTheme().resolveAttribute(android.R.attr.actionBarSize, typedValue, true)){
+            screen_h -= getResources().getDimensionPixelSize(typedValue.resourceId);
+        }
+        int x = (int) (Math.random() * screen_w);
+        int y = (int) (Math.random() * screen_h);
+        if (x + view.getWidth() > screen_w) {
+            x = screen_w - view.getWidth();
+        }
+        if (y + view.getWidth() > screen_h) {
+            y = screen_h - view.getWidth();
+        }
+        view.setY(y);
+        view.setX(x);
     }
 }
